bab5267c4c9c77543b81b28ee0cf4c7eab64967d
Singam Ravi Babu <Singam Ravi Babu>
2025-08-05 11:08:01 +0530

    this is second commit

---
+++ 002_tuple_accessing_items.py
@@ -0,0 +1,12 @@
+# create a tuple with five even numbers
+even_numbers = (2, 4, 6, 8, 10)
+
+# accessing items in a tuple
+item1 = even_numbers[0]  # accessing the first item
+print(item1)
+item2 = even_numbers[1]  # accessing the second item
+print(item2)
+item3 = even_numbers[4]  # accessing the fifth item
+print(item3)
+item_n = even_numbers[5]
+print(item_n)
