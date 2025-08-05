b744863499919f76991a92b8ba2e9b6783c2a12b
Singam Ravi Babu <Singam Ravi Babu>
2025-08-05 11:01:56 +0530

    first commit

---
+++ 001_tuple_intro.py
@@ -0,0 +1,12 @@
+# tuple
+# tuple is immutable object
+# tuple is a collection of objects
+# tuple is defined by using parentheses
+
+# example of tuple
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+
+# tuple with 5 South Indian Telugu names
+telugu_names = ("Chiranjeevi", "Mahesh Babu", "Prabhas", "Allu Arjun", "NTR")
+print(telugu_names)
